66df844d9faf847306bfb249467ed3f52635f3bd
Steve Brorens <sbrorens@gmail.com>
2020-09-10 08:43:16 +1200

    Added 'backlink' requirement

--- bot-script.py
+++ bot-script.py
@@ -3,23 +3,24 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 '''
-   bot-script.py - Intended to be run every day. Posts the lessons
-                   for the Linux Upskill Challenge into the subreddit 
-                   /r/linuxupskillchallenge - based on the text in 
-                   github snori74/linuxupskillchallenge.
+   bot-script.py - Run every day. Pulls the lessons for the Linux Upskill 
+                    Challenge from Github into the subreddit 
 
-    Note, don't run more that once a day, or you'll risk multple posting 
+    Note 1: Don't run more that once a day - you'll risk multiple posting 
+    Note 1a: Looks like we now have to generate a "Thoughts and comments 0 Day x" post too!
+    Note 2: Now with the added trick, (thanks u/Danny007dan), of inserting 
+    a "backlink" to the previous lesson just above the copyright notice,
+    like this:
+
+    ## PREVIOUS DAY'S LESSON
+    * [Day 3 - Power trip!](https://www.reddit.com/r/linuxupskillchallenge/comments/ip257g/day_3_power_trip/)
     
+    Which is cool, but how to we programattically get that link? Huh? Danny?
+    
+    '''
 
-'''
-import configparser
-import os
-import numpy as np
-import sys
-import json
 import praw
 import datetime
-from github import Github
 from functions import *
 from settings import *
 
@@ -47,19 +48,18 @@ def main():
         )
     
     if sys.argv[1] == "LIVE":    
-        subreddit = reddit.subreddit("linuxupskillchallenge")
-        print("Posting to the LIVE r/upskillchallenge...")
         today_date = datetime.date.today()
-        print("And working with today's date: ", today_date)
+        subreddit = reddit.subreddit("linuxupskillchallenge")
+        print("Posting to: r/upskillchallenge - with today's date: ", today_date)
     
     if sys.argv[1] == "TEST":
-        subreddit = reddit.subreddit("linuxupskillBotTest")
-        advert_subreddit = reddit.subreddit("linuxupskillBotTest")
+        TEST = True
+        subreddit = reddit.subreddit("LinuxUpSkillBotTest")
         print("In TESTing mode...")
 
         if len(sys.argv) > 2:
             today_date = datetime.datetime.strptime(sys.argv[2], '%Y-%m-%d')
-            print("And using date: ", today_date)
+            print("Using BOGUS date of: ", today_date)
         
         else:
             today_date = datetime.date.today()
@@ -69,15 +69,13 @@ def main():
     day_num, month_name, next_month = check_today(today_date)
     
     if day_num == 1:
-        #    On this day, we post and pin the standard "Day 1"
-        #    lesson - and the "short video" - and also repost
-        #    the "HOW THIS WORKS" text - but don't pin this, 
-        #    because only two posts can be pinned at a time.
+        #    Post and pin the standard "Day 1" lesson - and the ditto the "short video"
+        #    Repost "HOW THIS WORKS" text. Don't pin, cos only two can be at a time
         # 
         clear_all_pinned(subreddit)
         get_post_pin_day(subreddit, day_num)
         get_post_pin_file(subreddit, "day1-short-video.md")
-        get_post_file(subreddit, "how-this-works.md")    #   New post, but not pinned  
+        get_post_file(subreddit, "how-this-works.md")
         #
         #   clear last few of last month's lessons...
         delete_day(subreddit, 20)
@@ -93,15 +91,15 @@ def main():
         get_post_pin_day(subreddit, day_num)
         delete_day(subreddit, day_num - 4)
         #
-        #   refresh the "How this Works" post
+        #   then pull in the current "How this Works", and pin
         get_post_pin_file(subreddit, "how-this-works.md")
         #
         #    ...and post custom 'advert' messages to subreddits
-        get_post_advert(advert_subreddit, "linux")
-        get_post_advert(advert_subreddit, "linux4noobs")
-        get_post_advert(advert_subreddit, "linuxadmin")
-        get_post_advert(advert_subreddit, "linuxmasterrace")
-        get_post_advert(advert_subreddit, "sysadminiblogs")
+        get_post_advert(subreddit, "linux")
+        get_post_advert(subreddit, "linux4noobs")
+        get_post_advert(subreddit, "linuxadmin")
+        get_post_advert(subreddit, "linuxmasterrace")
+        get_post_advert(subreddit, "sysadminiblogs")
         
     elif day_num == None:
         print("No lesson today...")
--- functions.py
+++ functions.py
@@ -10,9 +10,11 @@ from settings import *
 def check_today(thisdate):
     '''
     Course "...runs from the first Monday of the month, and lasts for four weeks..."
+    
     Simple, but there are some surprising corner cases, e.g.:
      - the last day or two of <MONTH>'s course end up being in <MONTH+1> (e.g April 2020)
      - there's sometimes a whole week gap at the end of a course (e.g. June 2020)
+    
     '''
     delta = datetime.timedelta(days=1)
     delta7 = datetime.timedelta(days=7)
@@ -51,6 +53,7 @@ def check_today(thisdate):
     print("We're on day: ", day_num, " of the course")
     return([day_num,month_name, "June"])
 
+
 def get_setting(setting):
     '''
     Pull settings, including 'secrets', from local dot file
@@ -74,12 +77,22 @@ def get_setting(setting):
 
 
 def get_day(daynum):
+    '''
+    Works out the filename for the day's lesson, then
+    calls 'get_file' to pull directly from Github
+    '''
     import requests
-    filename = str(daynum).zfill(2) + ".md" # zfill adds leading zeros where needed
+    filename = str(daynum).zfill(2) + ".md"    #   Padding '1' to '01'
     title, body = get_file(filename)
     return([title, body])
 
+
 def get_file(filename):
+    '''
+    Given a filename, pulls lesson directly from Github (no API), in RAW 
+    format, then splits the title text off and tidies it - and returns 
+    both it and the now-trimmed body as a list.
+    '''
     import requests
     starturl = "https://raw.githubusercontent.com/snori74/linuxupskillchallenge/master/"
     url = starturl + filename
@@ -88,7 +101,7 @@ def get_file(filename):
     contents = (r.content)
     #   ...so we convert to string
     strcontent = contents.decode("utf-8")
-    #   title line off is everything before the newline...
+    #   title line is everything before the newline...
     title = (strcontent.partition('\n')[0])
     #   ...and the body is everything after.
     body = (strcontent.partition('\n')[2])
@@ -140,32 +153,61 @@ def post_to_linux():
     # post text as md to r/linux 
     pass
 
-def get_post_advert(advert_subreddit, subreddit_name):
+
+def get_post_advert(subreddit, subreddit_name):
     '''
-    A bit messy, but essentially we retrive a file for each of the other 
-    subreddits that we want to advertise in, and post it there - unless 
-    we're in TEST mode, in which case we post the adverts in the same test 
-    subreddit that we use for our main posts.
+   
+    We retrive a file for each of the other subreddits that we want to 
+    advertise in, and post into each of those subreddits - unless our "subreddit" 
+    is set to the test location "LinuxUpSkillBotTest" - in which case we also post
+    these adverts there too.
+    
+    Logic:
+   
+    get the file from Github, based on subreddit_name
+
+    if subreddit == "LinuxUpSkillBotTest":
+        post to subreddit 
+    else:
+        if subreddit == "LinuxUpSkillChallenge":
+        subreddit = reddit.subreddit(subreddit_name)
+        post to subreddit
+   
+    Note:
     
     The 'advert' text files are named in a very specific way:
     
            txt-for-linux-subreddit.md
            txt-for-sysadminblogs-reddit.md
-    '''
     
-    if advert_subreddit = "linuxupskillBotTest":
-        sr = "linuxupskillBotTest"
-    else:
-        sr = subreddit 
-
-    advert_file = "txt-for-" + subreddit + "-subreddit.md"
+    '''
+    advert_file = "txt-for-" + subreddit_name + "-subreddit.md"
     title, body = get_file(advert_file)
+    
+    if subreddit == "linuxupskillBotTest":
+        print("Posting: ", title)
+        post = sr.submit(title, selftext=body,
+            url=None, flair_id=None, flair_text=None,
+            resubmit=True, send_replies=True, nsfw=False, spoiler=False,
+            collection_id=None)
+
+    else:
+        if subreddit == "linuxupskillChallenge":
+            subreddit = reddit.subreddit(subreddit_name)
+            print("Posting: ", title)
+            post = sr.submit(title, selftext=body,
+                url=None, flair_id=None, flair_text=None,
+                resubmit=True, send_replies=True, nsfw=False, spoiler=False,
+                collection_id=None)
+
+
     print("Posting: ", title)
     post = sr.submit(title, selftext=body,
         url=None, flair_id=None, flair_text=None,
         resubmit=True, send_replies=True, nsfw=False, spoiler=False,
         collection_id=None)
 
+ 
 def info_on_subreddit(sr):
     print("Is this reddit ReadOnly?:", reddit.read_only)  # Output: False
     subreddit = sr
@@ -175,12 +217,15 @@ def info_on_subreddit(sr):
     print("title; ", subreddit.title)
     print("description: ", subreddit.description)
 
+
 def clear_all_pinned(subreddit):
+    print(type(subreddit), subreddit)
     for post in subreddit.new(limit=25):
         if post.stickied:
             print('Unpinning: ', post.title)
             post.mod.sticky(state=False) # THIS works!
             post.mod.distinguish(how='no')
+
 
 def post_sticky(subreddit, title, body):
     post = subbreddit.submit(title, selftext=body)
@@ -195,15 +240,18 @@ def post_sticky(subreddit, title, body):
         else:
             print('Still not showing as stickied')
 
+
 def delete_day(subreddit, day_num):
     title = "Day " + str(day_num) + " - "
     delete_title(subreddit, title)
+
 
 def delete_title(subreddit, title):
     for post in subreddit.new(limit=25):
         if post.title.startswith(title):
             print("Deleting: ", post.title)
             post.delete()
+
 
 def pin_title(subreddit, title):
     for post in subreddit.new(limit=25):
